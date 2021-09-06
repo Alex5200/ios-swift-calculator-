c9c25cb75a987b31546c7b9eb8c6ef88b425e94f
Alexandr Lyachov <55750683+Alex5200@users.noreply.github.com>
2021-09-06 16:10:43 +0300

    new iOS app app calculator

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var operand = "";
+    var numberOne = "";
+    var numberTwo = "";
+    
+    @IBOutlet weak var labelValue: UILabel!
+    
+   
+    @IBAction func button1(_ sender: UIButton) {
+        if operand.isEmpty{
+            labelValue.text = numberOne + (sender.titleLabel?.text)!
+            labelValue.text = numberOne
+        }else {
+            labelValue.text = numberTwo + (sender.titleLabel?.text)!
+            labelValue.text = numberTwo        }
     }
-
-
+    
+    @IBAction func buttonaction(_ sender: UIButton) {
+        operand = ""
+        numberTwo = ""
+        numberOne = ""
+        labelValue.text = "0"
+       
+    }
+    @IBAction func result(_ sender: UIButton) {
+        var resultData: Double = 0.0
+        
+        switch operand {
+        case "+":
+            resultData = Double(numberOne)! + Double(numberTwo)!
+        case "-":
+            resultData = Double(numberOne)! - Double(numberTwo)!
+        case "*":
+            resultData = Double(numberOne)! * Double(numberTwo)!
+        case "/":
+            resultData = Double(numberOne)! / Double(numberTwo)!
+        default:
+            print("non")
+        }
+    }
 }
+
 
