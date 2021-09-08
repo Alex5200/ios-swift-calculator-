255dc22c7b0b42453ce0a0f56a22d080ae4d0a11
Alex5200 <saha.aa35@gmail.com>
2021-09-08 14:58:29 +0300

    update git

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -14,16 +14,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var labelValue: UILabel!
     
-   
-    @IBAction func button1(_ sender: UIButton) {
+    @IBAction func InputNumber(_ sender: UIButton) {
+        switch numberOne {
+        case "1":
+            numberOne += "1";
+        case "2":
+            numberOne += "2";
+        case "3":
+            numberOne += "3";
+        case "4":
+            numberOne += "4";
+        case "5":
+            numberOne += "5";
+        case "6":
+            numberOne += "6";
+        case "7":
+            numberOne += "7";
+        case "8":
+            numberOne += "8";
+        case "9":
+            numberOne += "9";
+        case "0":
+            numberOne += "0";
+        default:
+            print("err")
+        }
         if operand.isEmpty{
             labelValue.text = numberOne + (sender.titleLabel?.text)!
             labelValue.text = numberOne
         }else {
             labelValue.text = numberTwo + (sender.titleLabel?.text)!
-            labelValue.text = numberTwo        }
+            labelValue.text = numberTwo
+
+        }
     }
-    
+    @IBAction func Operand(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as! String
+    }
     @IBAction func buttonaction(_ sender: UIButton) {
         operand = ""
         numberTwo = ""
@@ -31,8 +58,18 @@ class ViewController: UIViewController {
         labelValue.text = "0"
        
     }
+    @IBAction func InputValue(_ sender: UIButton) {
+       
+        if operand.isEmpty{
+            labelValue.text = numberOne + (sender.titleLabel?.text)!
+            labelValue.text = numberOne
+        }else {
+            labelValue.text = numberTwo + (sender.titleLabel?.text)!
+            labelValue.text = numberTwo
+
+        }    }
     @IBAction func result(_ sender: UIButton) {
-        var resultData: Double = 0.0
+        var resultData = 0.0
         
         switch operand {
         case "+":
@@ -46,7 +83,14 @@ class ViewController: UIViewController {
         default:
             print("non")
         }
-    }
-}
+        if resultData.truncatingRemainder(dividingBy: 1.0) == 0.0{
+            labelValue.text = String(Int(resultData))
+        }
+        else {
+            labelValue.text = String(resultData)
+
+        }
+        
+    }}
 
 
